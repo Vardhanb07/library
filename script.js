03323aaefc552e6bda265979aaec3f74d4e30189
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-03-16 14:37:14 +0530

    Add eventlistener to go-back button

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function addBookToLibrary(title, pages, status, priority){
     const id = crypto.randomUUID(); 
     myLibrary.push(new Book(title, pages, status, priority, id));
 }
-addBookToLibrary()
-
 const bookHolder = document.querySelector('.book-holder')
 const form = document.querySelector('.form')
 const header = document.querySelector('.header')
@@ -22,4 +20,22 @@ bookHolder.addEventListener('click', () => {
     bookHolder.style.display = 'none'
     form.style.display = 'block'
     header.style.filter = 'blur(8px)'
+})
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    let input = document.querySelector('#title').value
+    console.log(input)
+    form.style.display = 'none'
+    bookHolder.style.display = 'flex'
+    header.style.filter = 'blur(0)'
+})
+document.querySelector('.add-book').addEventListener('click', () => {
+    bookHolder.style.display = 'none'
+    form.style.display = 'block'
+    header.style.filter = 'blur(8px)'
+})
+document.querySelector('.return').addEventListener('click', () => {
+    bookHolder.style.display = 'flex'
+    form.style.display = 'none'
+    header.style.filter = 'blur(0)'
 })
